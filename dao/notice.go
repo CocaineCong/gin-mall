212532703b4c45b2569affb6415dbd0d61d2e8d2
cengqingcong <cengqingcong@shizhuang-inc.com>
2022-07-10 14:36:07 +0800

    fix: config

--- dao/notice.go
+++ dao/notice.go
@@ -18,7 +18,14 @@ func NewNoticeDaoByDB(db *gorm.DB) *NoticeDao {
 	return &NoticeDao{db}
 }
 
+// GetNoticeById 通过id获取notice
 func (dao *NoticeDao) GetNoticeById(id uint) (notice model.Notice, err error) {
 	err = dao.DB.Model(&model.Notice{}).Where("id=?", id).First(&notice).Error
+	return
+}
+
+// CreateNotice 创建notice
+func (dao *NoticeDao) CreateNotice(notice model.Notice) (err error) {
+	err = dao.DB.Model(&model.Notice{}).Create(&notice).Error
 	return
 }
