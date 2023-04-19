34e5e3ac1a0377cd7a0f0a658e778a942aa484cc
ozline <ozlinex@outlook.com>
2023-04-20 00:38:55 +0800

    add: consts/http_code.go

--- api/v1/product.go
+++ api/v1/product.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
-	"github.com/gin-gonic/gin"
+	"mall/consts"
 	util "mall/pkg/utils"
 	"mall/service"
+
+	"github.com/gin-gonic/gin"
 )
 
 // 创建商品
@@ -15,59 +17,59 @@ func CreateProduct(c *gin.Context) {
 	//c.SaveUploadedFile()
 	if err := c.ShouldBind(&createProductService); err == nil {
 		res := createProductService.Create(c.Request.Context(), claim.ID, files)
-		c.JSON(200, res)
+		c.JSON(consts.StatusOK, res)
 	} else {
-		c.JSON(400, ErrorResponse(err))
+		c.JSON(consts.IlleageRequest, ErrorResponse(err))
 		util.LogrusObj.Infoln(err)
 	}
 }
 
-//商品列表
+// 商品列表
 func ListProducts(c *gin.Context) {
 	listProductsService := service.ProductService{}
 	if err := c.ShouldBind(&listProductsService); err == nil {
 		res := listProductsService.List(c.Request.Context())
-		c.JSON(200, res)
+		c.JSON(consts.StatusOK, res)
 	} else {
-		c.JSON(400, ErrorResponse(err))
+		c.JSON(consts.IlleageRequest, ErrorResponse(err))
 		util.LogrusObj.Infoln(err)
 	}
 }
 
-//商品详情
+// 商品详情
 func ShowProduct(c *gin.Context) {
 	showProductService := service.ProductService{}
 	res := showProductService.Show(c.Request.Context(), c.Param("id"))
-	c.JSON(200, res)
+	c.JSON(consts.StatusOK, res)
 }
 
-//删除商品
+// 删除商品
 func DeleteProduct(c *gin.Context) {
 	deleteProductService := service.ProductService{}
 	res := deleteProductService.Delete(c.Request.Context(), c.Param("id"))
-	c.JSON(200, res)
+	c.JSON(consts.StatusOK, res)
 }
 
-//更新商品
+// 更新商品
 func UpdateProduct(c *gin.Context) {
 	updateProductService := service.ProductService{}
 	if err := c.ShouldBind(&updateProductService); err == nil {
 		res := updateProductService.Update(c.Request.Context(), c.Param("id"))
-		c.JSON(200, res)
+		c.JSON(consts.StatusOK, res)
 	} else {
-		c.JSON(400, ErrorResponse(err))
+		c.JSON(consts.IlleageRequest, ErrorResponse(err))
 		util.LogrusObj.Infoln(err)
 	}
 }
 
-//搜索商品
+// 搜索商品
 func SearchProducts(c *gin.Context) {
 	searchProductsService := service.ProductService{}
 	if err := c.ShouldBind(&searchProductsService); err == nil {
 		res := searchProductsService.Search(c.Request.Context())
-		c.JSON(200, res)
+		c.JSON(consts.StatusOK, res)
 	} else {
-		c.JSON(400, ErrorResponse(err))
+		c.JSON(consts.IlleageRequest, ErrorResponse(err))
 		util.LogrusObj.Infoln(err)
 	}
 }
@@ -76,9 +78,9 @@ func ListProductImg(c *gin.Context) {
 	var listProductImgService service.ListProductImgService
 	if err := c.ShouldBind(&listProductImgService); err == nil {
 		res := listProductImgService.List(c.Request.Context(), c.Param("id"))
-		c.JSON(200, res)
+		c.JSON(consts.StatusOK, res)
 	} else {
-		c.JSON(400, ErrorResponse(err))
+		c.JSON(consts.IlleageRequest, ErrorResponse(err))
 		util.LogrusObj.Infoln(err)
 	}
 }
---
+++ consts/http_code.go
@@ -0,0 +1,6 @@
+package consts
+
+const (
+	StatusOK       = 200 // OK
+	IlleageRequest = 400 // 非法请求
+)
