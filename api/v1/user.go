db13f467c7a42c9bc854e7c65f7b72757ad10a9f
FanOne <294350394@qq.com>
2023-04-22 11:01:24 +0800

    feat:user handler

--- api/v1/user.go
+++ api/v1/user.go
@@ -38,7 +38,6 @@ func UserLogin() gin.HandlerFunc {
 
 		if err := ctx.ShouldBind(&req); err == nil {
 			// 参数校验
-			userId := ctx.Keys["user_id"].(uint)
 			l := service.GetUserSrv()
 			resp, err := l.Login(ctx.Request.Context(), &req)
 			if err != nil {
@@ -54,51 +53,91 @@ func UserLogin() gin.HandlerFunc {
 	}
 }
 
-func UserUpdate(c *gin.Context) {
-	var userUpdateService service.UserService
-	claims, _ := util.ParseToken(c.GetHeader("Authorization"))
-	if err := c.ShouldBind(&userUpdateService); err == nil {
-		res := userUpdateService.Update(c.Request.Context(), claims.ID)
-		c.JSON(200, res)
-	} else {
-		c.JSON(400, ErrorResponse(err))
-		util.LogrusObj.Infoln(err)
+func UserUpdate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req types.UserServiceReq
+
+		if err := ctx.ShouldBind(&req); err == nil {
+			// 参数校验
+			userId := ctx.Keys["user_id"].(uint)
+			l := service.GetUserSrv()
+			resp, err := l.Update(ctx.Request.Context(), userId, &req)
+			if err != nil {
+				util.LogrusObj.Infoln(err)
+				ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusOK, resp)
+		} else {
+			util.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		}
 	}
 }
 
-func UploadAvatar(c *gin.Context) {
-	file, fileHeader, _ := c.Request.FormFile("file")
-	fileSize := fileHeader.Size
-	uploadAvatarService := service.UserService{}
-	chaim, _ := util.ParseToken(c.GetHeader("Authorization"))
-	if err := c.ShouldBind(&uploadAvatarService); err == nil {
-		res := uploadAvatarService.Post(c.Request.Context(), chaim.ID, file, fileSize)
-		c.JSON(200, res)
-	} else {
-		c.JSON(400, ErrorResponse(err))
-		util.LogrusObj.Infoln(err)
+func UploadAvatar() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req types.UserServiceReq
+
+		if err := ctx.ShouldBind(&req); err == nil {
+			// 参数校验
+			file, fileHeader, _ := ctx.Request.FormFile("file")
+			fileSize := fileHeader.Size
+			userId := ctx.Keys["user_id"].(uint)
+			l := service.GetUserSrv()
+			resp, err := l.Post(ctx.Request.Context(), userId, file, fileSize, &req)
+			if err != nil {
+				util.LogrusObj.Infoln(err)
+				ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusOK, resp)
+		} else {
+			util.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		}
 	}
 }
 
-func SendEmail(c *gin.Context) {
-	var sendEmailService service.SendEmailService
-	chaim, _ := util.ParseToken(c.GetHeader("Authorization"))
-	if err := c.ShouldBind(&sendEmailService); err == nil {
-		res := sendEmailService.Send(c.Request.Context(), chaim.ID)
-		c.JSON(200, res)
-	} else {
-		c.JSON(400, ErrorResponse(err))
-		util.LogrusObj.Infoln(err)
+func SendEmail() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req types.SendEmailServiceReq
+
+		if err := ctx.ShouldBind(&req); err == nil {
+			// 参数校验
+			userId := ctx.Keys["user_id"].(uint)
+			l := service.GetUserSrv()
+			resp, err := l.Send(ctx.Request.Context(), userId, &req)
+			if err != nil {
+				util.LogrusObj.Infoln(err)
+				ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusOK, resp)
+		} else {
+			util.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		}
 	}
 }
 
-func ValidEmail(c *gin.Context) {
-	var vaildEmailService service.ValidEmailService
-	if err := c.ShouldBind(vaildEmailService); err == nil {
-		res := vaildEmailService.Valid(c.Request.Context(), c.GetHeader("Authorization"))
-		c.JSON(200, res)
-	} else {
-		c.JSON(400, ErrorResponse(err))
-		util.LogrusObj.Infoln(err)
+func ValidEmail() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req types.ValidEmailServiceReq
+
+		if err := ctx.ShouldBind(&req); err == nil {
+			// 参数校验
+			l := service.GetUserSrv()
+			resp, err := l.Valid(ctx.Request.Context(), ctx.Param("token"), &req)
+			if err != nil {
+				util.LogrusObj.Infoln(err)
+				ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusOK, resp)
+		} else {
+			util.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		}
 	}
 }
