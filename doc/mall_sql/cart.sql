6f9f40098c93cf8805f696b2059daf8c1461b613
FanOne <294350394@qq.com>
2023-05-01 17:31:47 +0800

    fix:sql:

---
+++ doc/mall_sql/cart.sql
@@ -0,0 +1,3 @@
+insert into MY_TABLE (id, created_at, updated_at, deleted_at, user_id, product_id, boss_id, num, max_num, check)
+values  (1, '2023-01-22 14:29:36', '2023-01-23 05:05:17', null, 1, 2, 1, 2, 10, 0),
+        (2, '2023-01-23 05:04:58', '2023-01-23 05:04:58', null, 1, 1, 1, 1, 10, 0);
